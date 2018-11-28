4b1dc5e0a835315afe942e534fd4d26eca88c509
GlebDurygin <durygin.gleb@gmail.com>
2018-11-28 22:45:41 +0400

    add Constants.java, EmailAuthentificator.java, add dependency javax.mail

---
+++ src/main/java/com/emailbot/tmp/Constants.java
@@ -0,0 +1,10 @@
+package com.emailbot.tmp;
+
+public class Constants {
+
+    public static final String
+        AUTH_EMAIL = "mail@gmail.com",
+        AUTH_PASSWORD = "123445678i",
+        IMAP_SERVER_MAIL_RU = "imap.mail.ru",
+        IMAP_PORT = "993";
+}
--- src/main/java/com/emailbot/tmp/Main.java
+++ src/main/java/com/emailbot/tmp/Main.java
@@ -1,6 +1,20 @@
 package com.emailbot.tmp;
 
+import java.net.Authenticator;
+import java.util.Properties;
+
 public class Main {
     public static void main(String[] args){
+    }
+
+    public void ReadMail()
+    {
+        Properties properties = new Properties();
+        properties.put("mail.debug", "false");
+        properties.put("mail.store.protocol" , "imaps"  );
+        properties.put("mail.imap.ssl.enable", "true"   );
+        properties.put("mail.imap.port"      , Constants.IMAP_PORT);
+
+        //Authenticator authenticator = new EmailAuthentificator
     }
 }
